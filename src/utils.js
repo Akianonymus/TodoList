318daa44910f25ed923bf10d611e191ecd1de16b
Akianonymus <anonymus.aki@gmail.com>
2023-01-15 13:51:53 +0530

    Show username on Homepage

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 const API_URL =
   process.env.REACT_APP_BACKEND_SERVER ||
-  "https://aki-notes-app-backend.herokuapp.com";
+  "https://akitodolist-backend.cyclic.app";
 
 module.exports = API_URL;
